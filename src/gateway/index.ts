a7d356901591e52a42fb0714853ec7f20da45043
thaminuZs <170496648+thaminuZs@users.noreply.github.com>
2026-05-02 17:40:23 +0530

    solved elysia issue

--- src/gateway/index.ts
+++ src/gateway/index.ts
@@ -1,1 +1,11 @@
 import { Elysia } from "elysia";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = Number(process.env.PORT) || 5000;
+
+const app = new Elysia();
+
+app.listen(PORT, () => {
+  console.log(`uni portal is running on ${PORT}`);
+});
