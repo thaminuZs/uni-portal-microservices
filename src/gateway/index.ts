96f7e2bd10736e7e6d5cb4965b7e51a558049179
thaminuZs <170496648+thaminuZs@users.noreply.github.com>
2026-05-07 04:42:05 +0530

    gateway auth config

--- src/gateway/index.ts
+++ src/gateway/index.ts
@@ -3,6 +3,7 @@ import { cors } from "@elysiajs/cors";
 import { lecturerProxy } from "./routes/lecturer-proxy";
 import { canteenProxy } from "./routes/canteen-proxy";
 import { libraryProxy } from "./routes/library-proxy";
+import { authProxy } from "./routes/auth-proxy";
 
 if (process.env.NODE_ENV !== "production") {
   const dotenv = await import("dotenv");
@@ -10,7 +11,10 @@ if (process.env.NODE_ENV !== "production") {
 }
 const PORT = Number(process.env.PORT) || 5000;
 
-const app = new Elysia();
+const app = new Elysia()
+  .onError(({error, code}) => {
+    
+  });
 
 app.use(cors())
 
@@ -26,6 +30,7 @@ app.use(cors())
 
 .get("/", () => "gateway")
 
+.all("/api/auth*", (ctx) => authProxy(ctx), {parse: 'none'})
 .all("/api/lecturers*", (ctx) => lecturerProxy(ctx), {parse: 'none'})
 .all("/api/canteens*", (ctx) => canteenProxy(ctx), {parse: 'none'})
 .all("/api/libraries*", (ctx) => libraryProxy(ctx), {parse: 'none'})
