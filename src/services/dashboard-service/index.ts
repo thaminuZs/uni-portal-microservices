3ee13dc3a5352c6c3d39438ad591331728d43235
thaminuZs <170496648+thaminuZs@users.noreply.github.com>
2026-05-10 20:46:09 +0530

    dashboard setup 2

---
+++ src/services/dashboard-service/index.ts
@@ -0,0 +1,56 @@
+import { Elysia } from "elysia";
+
+const app = new Elysia();
+
+const PORT = process.env.PORT!;
+const lecturerUrl = process.env.LECTURER_URL!;
+const canteenUrl = process.env.CANTEEN_URL!;
+const libraryUrl = process.env.LIBRARY_URL!;
+
+const dashHeaders = {
+  "x-user-id": "dash-service",
+  "x-user-email": "dash@localmail.example",
+  "x-user-role": "dash",
+};
+
+app.get("/", async () => {
+  const lecturerRes = await (
+    await fetch(lecturerUrl, {
+      headers: dashHeaders,
+    })
+  ).json();
+
+  const canteenRes = await (
+    await fetch(canteenUrl, {
+      headers: dashHeaders,
+    })
+  ).json();
+
+  const libraryRes = await (
+    await fetch(libraryUrl, {
+      headers: dashHeaders,
+    })
+  ).json();
+
+  const presentCount = lecturerRes.data.filter(
+    (lecturer: any) => lecturer.status === "present"
+  ).length;
+
+  const presentQueue = canteenRes.data.filter(
+    (canteen: any) => canteen.currentQueue === "high"
+  ).length;
+
+  const presentCrowd = libraryRes.data.filter(
+    (library: any) => library.status === "full"
+  ).length;
+
+  console.log(presentCount, presentQueue, presentCrowd);
+
+  return {
+    lecturersPresent: presentCount,
+    crowdedCanteens: presentQueue,
+    crowdedLibraries: presentCrowd,
+  };
+});
+
+app.listen(PORT, () => console.log("server on"));
